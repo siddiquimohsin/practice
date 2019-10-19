8141e8788dac4b806d897858f2d60b39281e9b15
Hadir Khan <hadirkhan@hotmail.com>
2019-10-19 18:34:52 +0500

    Removed unnecessary files

--- chisel-tutorial/src/test/scala/examples/MultiplierTests.scala
+++
@@ -1,9 +0,0 @@
-package examples
-import chisel3.iotesters.PeekPokeTester
-
-class MultiplierTests(c: Multiplier) extends PeekPokeTester(c) {
-    poke(c.io.a, 2)
-    poke(c.io.b, 3)
-    step(1)
-    expect(c.io.r, 6)
-}
